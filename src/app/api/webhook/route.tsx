bdd92250d8dc541a6bfe00a865a688ee25f71a46
bennykok <itechbenny@gmail.com>
2024-10-25 21:58:13 +0800

    fix

--- src/app/api/webhook/route.tsx
+++ src/app/api/webhook/route.tsx
@@ -2,13 +2,11 @@ import { db } from "@/db/db";
 import { runs } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
-import { RunUpdateRequestBodyWebhookPostBody$inboundSchema as WebhookParser } from "comfydeploy/models/webhooks";
+import { WorkflowRunWebhookBody$inboundSchema as WebhookParser } from "comfydeploy/models/components";
 
 export async function POST(request: Request) {
 	console.log("webhook");
-	const parseData = WebhookParser.safeParse(
-		await request.json(),
-	);
+	const parseData = WebhookParser.safeParse(await request.json());
 	console.log("webhook", parseData, parseData.error?.toString());
 
 	if (!parseData.success) {
@@ -20,14 +18,17 @@ export async function POST(request: Request) {
 	const { status, runId, outputs, liveStatus } = data;
 
 	if (status === "success") {
-		const imageUrl = outputs?.[0].data?.images?.[0].url;
-		await db
-			.update(runs)
-			.set({
-				image_url: imageUrl,
-			})
-			.where(eq(runs.run_id, runId));
-		console.log("updated", runId, imageUrl);
+		const data = outputs?.[0].data?.images?.[0];
+		if (data && typeof data !== "string") {
+			const imageUrl = data.url;
+			await db
+				.update(runs)
+				.set({
+					image_url: imageUrl,
+				})
+				.where(eq(runs.run_id, runId));
+			console.log("updated", runId, imageUrl);
+		}
 	}
 
 	// Do your things
