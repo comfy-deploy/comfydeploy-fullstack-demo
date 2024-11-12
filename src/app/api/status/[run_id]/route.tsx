5693dd65a856cda95ae2b5eda4143a4720d71ce3
Apolonio420 <vwnrvm@gmail.com>
2024-11-12 15:22:20 -0300

    Update route.tsx

--- src/app/api/status/[run_id]/route.tsx
+++ src/app/api/status/[run_id]/route.tsx
@@ -1,10 +1,10 @@
+import { NextResponse } from "next/server";
 import { db } from "@/db/db";
 import { runs } from "@/db/schema";
-import { NextRequest, NextResponse } from "next/server";
 import { eq } from "drizzle-orm";
 
 export async function GET(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { run_id: string } }
 ) {
   const { run_id } = params;
