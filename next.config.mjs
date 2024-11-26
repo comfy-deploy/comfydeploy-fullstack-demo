e60145335b2ff1cab5bf502fca3b101eb91fb273
Apolonio420 <vwnrvm@gmail.com>
2024-11-26 17:20:18 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Alias 'react-konva' to use the core version
+      config.resolve.alias['react-konva'] = 'react-konva/lib/ReactKonvaCore';
+  
+      if (isServer) {
+        // Exclude 'konva' and 'canvas' from the server build
+        config.externals.push({
+          'konva': 'konva',
+          'canvas': 'canvas',
+        });
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
