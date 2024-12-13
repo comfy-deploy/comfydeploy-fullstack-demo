f7ec358926ae0e1bb268758b3c70acfe01819ac8
bennykok <itechbenny@gmail.com>
2024-12-13 15:06:10 +0800

    update webhook

--- src/app/api/webhook/route.tsx
+++ src/app/api/webhook/route.tsx
@@ -2,20 +2,14 @@ import { db } from "@/db/db";
 import { runs } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
-import { WorkflowRunWebhookBody$inboundSchema as WebhookParser } from "comfydeploy/models/components";
+import { ComfyDeploy } from "comfydeploy";
+
+const cd = new ComfyDeploy();
 
 export async function POST(request: Request) {
-	console.log("webhook");
-	const parseData = WebhookParser.safeParse(await request.json());
-	console.log("webhook", parseData, parseData.error?.toString());
+	const data = await cd.validateWebhook({ request });
 
-	if (!parseData.success) {
-		return NextResponse.json({ message: "error" }, { status: 400 });
-	}
-
-	const data = parseData.data;
-
-	const { status, runId, outputs, liveStatus } = data;
+	const { status, runId, outputs, liveStatus, progress } = data;
 
 	if (status === "success") {
 		const data = outputs?.[0].data?.images?.[0];
