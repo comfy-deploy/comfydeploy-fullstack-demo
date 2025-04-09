56699e95f0e7082b0a61c4b02dffe7c9eac4d4ba
bennykok <itechbenny@gmail.com>
2025-04-10 00:43:50 +0300

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,9 @@ export default {
 	out: './migrations',
 	schema: './src/db/schema.ts',
 	breakpoints: true,
-	driver: 'libsql',
 	dbCredentials: {
 		url: process.env.DATABASE_URL,
 	},
+	dialect: 'turso'
 } satisfies Config;
 
