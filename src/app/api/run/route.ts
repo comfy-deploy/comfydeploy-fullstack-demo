084c7cf7512d8efba03e2752507daf55dee6eb1b
bennykok <itechbenny@gmail.com>
2025-04-10 00:52:24 +0300

    debug

--- src/app/api/run/route.ts
+++ src/app/api/run/route.ts
@@ -59,8 +59,8 @@ export async function POST(request: NextRequest) {
 	})
 
 	if (data.status !== 200) {
-		console.log(data.statusText);
-		return NextResponse.json({ error: "Failed to create run" }, { status: 500 })
+		const text = await data.text()
+		return NextResponse.json({ error: "Failed to create run: " + text }, { status: 500 })
 	}
 
 	const json = await data.json()
